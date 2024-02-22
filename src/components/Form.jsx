9fab76d86dd31e4b46c1c8071e02e65eb0950865
DeeptenduKuri <deeptendukuri@gmail.com>
2024-02-22 14:26:20 +0530

    Continue

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -61,19 +61,19 @@ export default function Form({ handleClose }) {
           <h2>Fill Details</h2>
           <>
             <label htmlFor="name"><strong>Username:</strong></label>
-            <input onChange={(e) => setFormData({ ...formData, name: e.target.value })} value={formData.name} name="name" />
+            <input id="username" onChange={(e) => setFormData({ ...formData, name: e.target.value })} value={formData.name} name="name" />
           </>
           <>
             <label htmlFor="email"><strong>Email:</strong></label>
-            <input onChange={(e) => setFormData({ ...formData, email: e.target.value })} value={formData.email} name="email" />
+            <input id="email" onChange={(e) => setFormData({ ...formData, email: e.target.value })} value={formData.email} name="email" />
           </>
           <>
             <label htmlFor="number"><strong>Number:</strong></label>
-            <input onChange={(e) => setFormData({ ...formData, number: e.target.value })} value={formData.number} name="number" />
+            <input id="phone" onChange={(e) => setFormData({ ...formData, number: e.target.value })} value={formData.number} name="number" />
           </>
           <>
             <label htmlFor="dob"><strong>Date of Birth:</strong></label>
-            <input onChange={(e) => setFormData({ ...formData, dob: e.target.value })} value={formData.dob} name="dob" type="date" />
+            <input id="dob" onChange={(e) => setFormData({ ...formData, dob: e.target.value })} value={formData.dob} name="dob" type="date" />
           </>
           <button className="btn" type="submit">Submit</button>
         </form>
